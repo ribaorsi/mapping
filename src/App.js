9d1eafb9eaf11c116da8944d0b2cf446b59dfbf5
ribaorsi <ribaorsi@gmail.com>
2022-02-22 13:55:59 +0100

    mentés

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const customers = [
+  {
+      id: 1,
+      name: "Kristóf",
+      favouriteProducts: [
+      {
+          category: "tv",
+          color: "white"
+      },
+      {
+        category: "tv",
+        color: "white"
+    }
+  ]
+  },
+
+  {
+      id: 2,
+      name: "Ferenc",
+      favouriteProducts: [
+          {
+              category: "headphone",
+              color: "black"
+          }
+      ]
+  }
+]
+
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <ul>
+      {customers.map((customerObject) => (
+      <li key={customerObject.id} >
+        <strong>{customerObject.name}</strong>
+        
+        {customerObject.favouriteProducts.map((favProductObject) => (
+  <div key={favProductObject.category}>
+    <p>{favProductObject.category}</p>
+    <p>{favProductObject.color}</p>
+  </div>
+  ))}
+        
+      </li>
+    ))}
+    
+    </ul>
+
+    
+  </div>
+  )
 }
 
 export default App;
